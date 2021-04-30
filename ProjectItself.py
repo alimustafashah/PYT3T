94406f5856464daabd63def38893cb3486e682cb
Dan <79444147+DDSNA@users.noreply.github.com>
2021-04-30 13:35:03 +0300

    Bugfixes

--- ProjectItself.py
+++ ProjectItself.py
@@ -1,7 +1,7 @@
 import time
 
 print('This is a test file')
-name = input('What is your name?')
+name = input('What is your name? Type here: ')
 
 if len(name) > 50:
     print("Name must contain less than 50 characters!")
@@ -43,28 +43,49 @@ time.sleep(1)
 
 print(f"We are happy to have you!")
 print("")
+time.sleep(1)
+
 print("Get to work!")
+time.sleep(1)
 print('But first help me with this: ')
 
 secret_number = 2
 guess_count = 0
 guess_limit = 5
 while guess_count < guess_limit:
-    guess = int(input("Try to guess my secret number! You have 5 tries. It`s between 5 and 0: "))
-    guess_count += 1
-    if guess == secret_number:
-        print(f"Congratulations, {name}! You win!")
-        break
-    else:
-        print(f"Nope, {name} you got it wrong. Try again!")
-else:
-    print("Sorry, you failed!")
-time.sleep(5)
+    try:
+        guess = int(input("Try to guess my secret number! You have 5 tries. It`s between 5 and 0: "))
+        guess_count += 1
+        if guess == secret_number:
+            print(f"Congratulations, {name}! You win!")
+            break
+        else:
+            print(f"Nope, {name} you got it wrong. Try again!")
+    except guess_count == guess_limit:
+        print("Sorry, you failed!")
+
 
 class Human:
+    def __init__(self, name):
+        self.name = name
+
     def write(self):
-        print("This a human, writing.")
+        print(f"This is not {self.name}, writing.")
+
+    def read(self):
+        print(f"This is {self.name}, reading")
+
+
 class Dan(Human):
     def read(self):
         print("This is Dan, a human reading this text (probably!)")
+
+
+#user = Human({name})
+#print(user.name)
+#user.read()
+
+time.sleep(5)
+
+
 
